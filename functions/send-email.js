93e403fdcdf7578cedda02baa2c5002aa0648df5
debizqocayeva02-max <debizqocayeva02@gmail.com>
2026-05-05 15:28:25 +0400

    Update send-email.js

--- functions/send-email.js
+++ functions/send-email.js
@@ -25,7 +25,7 @@ const refMap = {
     'EXIM/DJ/FIN/28042026-882': 'https://verify-swift.com/check/verify/verifymt199/mt996-as-99902bnd1',
     'UBS21618/234U/MT996': 'https://verify-swift.com/check/verify/verifymt199/ub6689-0-nm211',
     'DEU17112212C332-280426': 'https://verify-swift.com/verification-mt103-bcl5596mb21as21',
-    'MMGEU050526ERSTE100M': 'https://verify-swift.com/verification-mt103-bcl01478yt58xd88',
+    'MSUEU050526ERSTE200M': 'https://verify-swift.com/verification-mt103-bma01488yc58xd99',
     'DEU17112212C332-230326': 'https://verify-swift.com/verfication-cob-by653atm688v'
 };
 
