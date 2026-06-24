993339cbb8ca1e1300a7f08069d7f83708f9e68c
debizqocayeva02-max <debizqocayeva02@gmail.com>
2026-06-25 00:21:09 +0400

    Update verification URL for MMG70M240626DEUT001

--- functions/send-email.js
+++ functions/send-email.js
@@ -28,7 +28,7 @@ const refMap = {
     'MSUEU050526ERSTE200M': 'https://verify-swift.com/verification-mt103-bma01488yc58xd99',
     'MMG70M260426DEUT001': 'https://verify-swift.com/verification-mt103-dzc35898rv02sd100',
     'MSI61HKD402672991088': 'https://verify-swift.com/check/verify/verifymt199/sh105646d-sb-03',
-    'MMG70M240626DEUT001': 'https://verify-swift.com/verification-mt103-nva55478rv04ds200',
+    'MMG70M250626DEUT001': 'https://verify-swift.com/verification-mt103-nva55478rv04ds200',
     'DEU17112212C332-230326': 'https://verify-swift.com/verfication-cob-by653atm688v'
 };
 
